5822acfa7761ae5d3397db76fcc59280f30d0f4e
Abhishek Choubey <your_email@example.com>
2026-07-17 07:58:08 +0530

    Fix Brevo sender

--- app/mail.py
+++ app/mail.py
@@ -1,11 +1,9 @@
 import os
 import random
 import requests
-
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 EMAIL = os.getenv("EMAIL_ADDRESS")
 BREVO_API_KEY = os.getenv("BREVO_API_KEY")
@@ -16,6 +14,9 @@ def generate_otp():
 
 
 def send_otp(receiver_email, otp):
+
+    print("EMAIL_ADDRESS:", EMAIL)
+    print("BREVO_API_KEY Loaded:", "YES" if BREVO_API_KEY else "NO")
 
     subject = "Smart Inventory - Password Reset OTP"
 
@@ -41,11 +42,11 @@ Regards,
 Smart Inventory Team
 """
 
-
     data = {
         "sender": {
             "name": "Smart Inventory",
-            "email": EMAIL
+            # Verified sender
+            "email": "abhishekchoubey012@gmail.com"
         },
         "to": [
             {
@@ -56,34 +57,31 @@ Smart Inventory Team
         "textContent": body
     }
 
-
     headers = {
         "accept": "application/json",
         "api-key": BREVO_API_KEY,
         "content-type": "application/json"
     }
 
-
     try:
 
         response = requests.post(
             "https://api.brevo.com/v3/smtp/email",
             json=data,
-            headers=headers
+            headers=headers,
+            timeout=30
         )
 
+        print("Status Code:", response.status_code)
+        print("Brevo Response:", response.text)
 
         if response.status_code == 201:
+            print("OTP Email Sent Successfully")
             return True
 
-
-        print("Brevo Error :", response.text)
-
+        print("Brevo Error:", response.text)
         return False
 
-
     except Exception as e:
-
-        print("Email Error :", e)
-
+        print("Email Error:", str(e))
         return False
